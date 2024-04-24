9e490de0c460b442531064b5f702144e63c9350a
hardcorekid03 <parasapremiumko88@gmail.com>
2024-04-24 15:59:24 +0800

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import "./assets/dist/js/bootstrap.bundle.min.js";
-import "./assets/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes.jsx";
 import Header from "./components/header/Header";
